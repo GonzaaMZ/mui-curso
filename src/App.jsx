1c740adc449eceb40cc1ca4828efb12358d1a47a
Gonzalo Amaya <86499412+GonzaaMZ@users.noreply.github.com>
2023-06-14 00:56:57 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const App = () => {
+  return ( 
+    <>
+    <h1>App</h1>
+    <Button> Mi Primer Boton</Button>
+    </>
+   );
+}
+ 
+export default App;
